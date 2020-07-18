59b96e83437928469fac58d7ba859b70754c0cb7
JohnCorby <williamforgames6@gmail.com>
2020-07-17 18:53:10 -0700

    ready to rumble

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::Read;
+
+use bincode::deserialize;
+use serde::Deserialize;
+
 fn main() {
-    println!("Hello, world!");
+    const PATH: &str = "data/AudioRes/Stream/SMG_astrodome02_strm.ast";
+
+    let mut file = File::open(PATH).expect("error opening file");
+    let mut encoded = Vec::new();
+    file.read_to_end(&mut encoded).expect("error reading file");
+
+    let decoded: ASTFile = deserialize(&encoded).expect("error deserializing file");
+    println!("{:#?}", decoded)
 }
+
+
+#[derive(Deserialize, Debug)]
+struct ASTFile {}
